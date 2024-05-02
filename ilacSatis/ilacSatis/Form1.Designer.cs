010df1c90cfc209aeb356c5b0e9eb928ef25cffe
gsenakrgzl <karaguzelsena46@gmail.com>
2024-05-02 11:20:03 +0300

    IlacSatis
    C# ve MySql kullanılarak geliştirildi.

---
+++ ilacSatis/ilacSatis/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace ilacSatis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbilacAdi = new System.Windows.Forms.TextBox();
+            this.tbİlacKodu = new System.Windows.Forms.TextBox();
+            this.tbMiktar = new System.Windows.Forms.TextBox();
+            this.tbURL = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnSatis = new System.Windows.Forms.Button();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.resim = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.OldLace;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(30, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "İLAÇ SATIŞ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(30, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "İlaç Adı :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(30, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "İlaç Kodu :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(30, 199);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Miktarı :";
+            // 
+            // tbilacAdi
+            // 
+            this.tbilacAdi.Location = new System.Drawing.Point(141, 91);
+            this.tbilacAdi.Name = "tbilacAdi";
+            this.tbilacAdi.Size = new System.Drawing.Size(100, 22);
+            this.tbilacAdi.TabIndex = 4;
+            // 
+            // tbİlacKodu
+            // 
+            this.tbİlacKodu.Location = new System.Drawing.Point(141, 140);
+            this.tbİlacKodu.Name = "tbİlacKodu";
+            this.tbİlacKodu.Size = new System.Drawing.Size(100, 22);
+            this.tbİlacKodu.TabIndex = 5;
+            // 
+            // tbMiktar
+            // 
+            this.tbMiktar.Location = new System.Drawing.Point(141, 197);
+            this.tbMiktar.Name = "tbMiktar";
+            this.tbMiktar.Size = new System.Drawing.Size(48, 22);
+            this.tbMiktar.TabIndex = 6;
+            // 
+            // tbURL
+            // 
+            this.tbURL.Location = new System.Drawing.Point(395, 197);
+            this.tbURL.Name = "tbURL";
+            this.tbURL.Size = new System.Drawing.Size(212, 22);
+            this.tbURL.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(395, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(212, 34);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "URL\'den Resim Ekle";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(395, 139);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(212, 35);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Klasörden Resim Ekle";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.Location = new System.Drawing.Point(18, 279);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(122, 32);
+            this.btnEkle.TabIndex = 10;
+            this.btnEkle.Text = "İlaç Ekle";
+            this.btnEkle.UseVisualStyleBackColor = false;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnSatis
+            // 
+            this.btnSatis.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnSatis.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSatis.Location = new System.Drawing.Point(165, 279);
+            this.btnSatis.Name = "btnSatis";
+            this.btnSatis.Size = new System.Drawing.Size(121, 32);
+            this.btnSatis.TabIndex = 11;
+            this.btnSatis.Text = "Satış Yap";
+            this.btnSatis.UseVisualStyleBackColor = false;
+            this.btnSatis.Click += new System.EventHandler(this.btnSatis_Click);
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnTemizle.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnTemizle.Location = new System.Drawing.Point(309, 279);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(160, 32);
+            this.btnTemizle.TabIndex = 12;
+            this.btnTemizle.Text = "Tabloyu Temizle";
+            this.btnTemizle.UseVisualStyleBackColor = false;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnSil.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSil.Location = new System.Drawing.Point(491, 279);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(116, 32);
+            this.btnSil.TabIndex = 13;
+            this.btnSil.Text = "Satır Sil";
+            this.btnSil.UseVisualStyleBackColor = false;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(630, 90);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(146, 145);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(18, 333);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(758, 217);
+            this.dataGridView1.TabIndex = 15;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(226, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 16);
+            this.label5.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(540, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 16);
+            this.label6.TabIndex = 17;
+            // 
+            // resim
+            // 
+            this.resim.AutoSize = true;
+            this.resim.Location = new System.Drawing.Point(409, 235);
+            this.resim.Name = "resim";
+            this.resim.Size = new System.Drawing.Size(0, 16);
+            this.resim.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 562);
+            this.Controls.Add(this.resim);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnTemizle);
+            this.Controls.Add(this.btnSatis);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbURL);
+            this.Controls.Add(this.tbMiktar);
+            this.Controls.Add(this.tbİlacKodu);
+            this.Controls.Add(this.tbilacAdi);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbilacAdi;
+        private System.Windows.Forms.TextBox tbİlacKodu;
+        private System.Windows.Forms.TextBox tbMiktar;
+        private System.Windows.Forms.TextBox tbURL;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnSatis;
+        private System.Windows.Forms.Button btnTemizle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label resim;
+    }
+}
+
---
+++ ilacSatis/ilacSatis/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace ilacSatis
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection baglanti = new MySqlConnection("server = localhost; uid = root; database = eczane");
+        MySqlCommand komut;
+        MySqlDataAdapter adaptor;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label5.Text = DateTime.Now.ToLongDateString();
+            label6.Text = DateTime.Now.ToLongTimeString();
+            timer1.Start();
+            listele();
+
+        }
+
+        private void listele()
+        {
+            adaptor = new MySqlDataAdapter("Select * From ilactablo",baglanti);
+            DataTable tablo = new DataTable();
+            adaptor.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            tbilacAdi.Text = "";
+            tbİlacKodu.Text = "";
+            tbMiktar.Text = "";
+            tbURL.Text = "";
+            pictureBox1.Image = null;
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            komut = new MySqlCommand("Insert Into ilactablo (ilac_adi, ilac_kodu, stok, resim) values ('" + tbilacAdi.Text + "', '" + int.Parse(tbİlacKodu.Text) + "', '" + int.Parse(tbMiktar.Text) + "', @resim)", baglanti);
+            baglanti.Open();
+            komut.Parameters.AddWithValue("resim",resim.Text);
+            komut.ExecuteNonQuery();
+            baglanti.Clone();
+            listele();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string urlresim = tbURL.Text;
+            pictureBox1.Load(urlresim);
+            resim.Text = tbURL.Text;
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            tbilacAdi.Text= dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            tbİlacKodu.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            tbMiktar.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            tbURL.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+
+            if (dataGridView1.CurrentRow.Cells[4].Value.ToString().Contains("http"))
+            {
+                pictureBox1.Load(dataGridView1.CurrentRow.Cells[4].Value.ToString());
+            }
+            else
+            {
+                pictureBox1.ImageLocation = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "Resim Dosyaları |* .jpg; * .jpeg; * .png";
+            fd.ShowDialog();
+            pictureBox1.ImageLocation= fd.FileName;
+            resim.Text = fd.FileName.ToString();
+        }
+
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+            komut = new MySqlCommand("Delete from ilactablo", baglanti);
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            listele();
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            komut = new MySqlCommand("Delete from ilactablo where ilac_adi = '"+ dataGridView1.CurrentRow.Cells[1].Value.ToString() + "'",baglanti);
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            listele();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label5.Text = DateTime.Now.ToLongDateString();
+            label6.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        private void btnSatis_Click(object sender, EventArgs e)
+        {
+            int gecici = int.Parse(dataGridView1.CurrentRow.Cells[3].Value.ToString());
+            gecici = gecici - 1;
+            komut = new MySqlCommand("Update ilactablo set stok = '"+gecici+"' where ilac_adi = '"+ dataGridView1.CurrentRow.Cells[1].Value.ToString() + "'", baglanti);
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();  
+            listele();
+        }
+    }
+}
